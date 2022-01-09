83ce6c86c39ffd24df69e24c946c7d9dcb6b8f1d
HeloiseMacena <42615847+HeloiseMacena@users.noreply.github.com>
2022-01-09 18:01:49 +0000

    menu

--- pokedex/main.cs
+++ pokedex/main.cs
@@ -2,13 +2,36 @@ using System;
 
 class MainClass{
   public static void Main(){
-    
+    bool loop = true;
     Type grass = new Type(2,"Grass");
+
     Pokemon poke1 = new Pokemon(1,"Bulbasaur", 0.7, 6.9, 45, 49, 49, 65, 65, 45, grass);
     Pokemon poke2 = new Pokemon(2,"Ivysaur", 1.0, 13.0, 60, 62, 63, 80, 80, 60, grass);
 
-    Console.WriteLine(poke1);
-    Console.WriteLine(poke2);
 
+    Console.Clear();
+    while(loop) {
+      Console.WriteLine("1 - Cadastrar Tipo");
+      Console.WriteLine("2 - Cadastrar Pokemon");
+      Console.WriteLine("3 - Sair");
+
+      int res = int.Parse(Console.ReadLine());
+      switch(res) {
+        case 1: 
+          Console.WriteLine("Tipo");
+          break;
+        case 2:
+          Console.WriteLine("Pokemon");
+          break;
+        case 3:
+          loop = false;
+          Console.Clear();
+          Console.WriteLine("Você saiu!");
+          break;
+        default:
+          Console.WriteLine("Opção inválida");
+          break;
+      }
+    }
   }
 }
