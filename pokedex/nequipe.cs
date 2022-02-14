616003d85be5596f3b49fb2a6a072391f0eae379
DiogoSamnas <diogo.sam.nascimento@gmail.com>
2022-02-14 09:39:17 -0300

    Classes de Negócio Equipe

---
+++ pokedex/nequipe.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+class NEquipe{
+  private List<Equipe> equipes = new List<Equipe>();
+
+  // Operações
+  public List<Equipe> Listar(){
+    // Retorna uma lista com as equipes cadastradas
+    return equipes;
+  }
+
+  public List<Equipe> Listar(User u){
+    // Retorna uma lista com as equipes cadastradas do usuário u
+    List<Equipe> es = new List<Equipe>();
+    foreach(Equipe e in equipes)
+      if(e.GetUser() == u) es.Add(e);
+    return es;
+  }
+
+  public Equipe ListarEquipe(User u){
+    // Retorna a equipe do usuário u
+    foreach(Equipe e in equipes)
+      if(e.GetUser() == u) return e;
+    return null;
+  }
+
+  public void Inserir(Equipe e){
+    //gerar o id da equipe
+    int max = 0;
+    foreach(Equipe obj in equipes)
+      if(obj.GetId() > max) max = obj.GetId();
+    e.SetId(max + 1);
+    //Inserir equipe na lista de equipes
+    equipes.Add(e);
+  }
+
+  public List<EquipePokemon> EquipePokemonListar(Equipe e){
+    // Retorna os pokemons da equipe
+    return e.EquipePokemonListar();
+  }
+
+  public void EquipePokemonInserir(Equipe e, Pokemon p){
+    // Inserir um pokemon na lista
+    e.EquipePokemonInserir(p);
+  }
+
+  public void EquipePokemonExcluir(Equipe e){
+    // Remove todos os pokemons da equipe
+    e.EquipePokemonExcluir();
+  }
+}
